99ddb65454cf50e908bc8139530a90edc3143cb9
Vinisilva0010 <vns.pontual77@gmail.com>
2026-08-28 18:55:45 -0300

    robot

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,33 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = "https://zanvendas.zanvexis.com";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/", "/admin/", "/go/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/admin/", "/go/"],
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "Google-Extended",
+        allow: "/",
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl,
   };
